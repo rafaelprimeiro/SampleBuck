2841fe7ea8b608fcf1d44631485bc23e9471c90d
Rafael Gabriel <git@rafaelgabriel.dev>
2021-08-25 20:25:23 -0300

    moved project files

---
+++ App/BuckSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuckSample
+//
+//  Created by Rafael Gabriel on 25/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
